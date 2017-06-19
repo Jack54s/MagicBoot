ee38f1d5e8c5fb1a1d621007a541338de48e8dcd
11407 <11407@REDWEB>
2017-06-19 09:47:55 +0800

    只能启动一个实例

--- CommandStartProgram/Program.cs
+++ CommandStartProgram/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 
 namespace MagicBoot
@@ -15,9 +14,59 @@ namespace MagicBoot
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            bool createNew;
+            using(System.Threading.Mutex mutex = new System.Threading.Mutex(true, Application.ProductName, out createNew))
+            {
+                if (createNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    Process instance = RunningInstance();
+                    if(instance != null)
+                    {
+                        HandleRunningInstance(instance);
+                    }
+                    else
+                    {
+                        MessageBox.Show("未知错误，请先退出正在运行的程序！");
+                    }
+                }
+            }
         }
+
+        private static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            foreach(Process process in processes)
+            {
+                if(process.Id != current.Id)
+                {
+                    if(System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("/", @"/") == current.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static void HandleRunningInstance(Process instance)
+        {
+            IntPtr windowHandle = FindWindow(null, "MagicBoot");
+            ShowWindow(windowHandle, 1);
+            SetForegroundWindow(windowHandle);
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
     }
 }
